003da1ed5f4cc4ad8f44c6e7c8232856be7d996b
lamberj1986 <lamberj1986@gmail.com>
2024-10-01 22:20:10 -0400

    Prep work for week 3

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -25,6 +25,17 @@
     private static int CountDuplicates(int[] data)
     {
         // Add code here.
-        return 0;
+        var unique = new HashSet<int>();
+        var dupCount = 0;
+
+        foreach (var x in data)
+        {
+            if (unique.Contains(x))
+                dupCount++;
+            else
+                unique.Add(x);
+        }
+
+        return dupCount;
     }
 }
