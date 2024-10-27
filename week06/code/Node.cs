790a96cb55aafca85281414e4c58985383e9845b
lamberj1986 <lamberj1986@gmail.com>
2024-10-26 20:29:26 -0400

    Updates for week 6 programming.

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -13,6 +13,9 @@ public class Node
     {
         // TODO Start Problem 1
 
+        if (value == Data)
+            return; // Value already exists, do nothing
+
         if (value < Data)
         {
             // Insert to the left
@@ -34,12 +37,23 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
+        if (value == Data)
+            return true;
+
+        if (value < Data && Left is not null)
+            return Left.Contains(value);
+
+        if (value > Data && Right is not null)
+            return Right.Contains(value);
+
         return false;
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
